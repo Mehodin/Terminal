224240ea13e0e000f9366fa7401af907df83cb8c
Mehodin <32092714+Mehodin@users.noreply.github.com>
2018-05-26 01:32:09 +0200

    Initial push

---
+++ ClassHolder.py
@@ -0,0 +1,12 @@
+class Variables:
+	Variables1 = [5, 10, 15, 20]
+	Variables2 = {'Key1':12,'Key5':'Terminal rocks!'}
+  
+class Calculations:
+	def AddUp(Value1, Value2):
+		AddedUp = Value1 + Value2
+		return AddedUp
+		
+	def ModulusNumbers(Value1, Value2):
+		Modulus = Value1 % Value2
+		return Modulus
---
+++ FunctionCaller.py
@@ -0,0 +1,7 @@
+import ClassHolder
+
+Value1 = ClassHolder.Variables.Variables2['Key1']
+Value2 = ClassHolder.Variables.Variables1[0]
+
+ValueResult = ClassHolder.Calculations.AddUp(Value1, Value2)
+print(ValueResult)
